5eb7f2b4293e366168b41d17b64e72a6fe8d1a8c
jsnascimento <jardel.snascimento@gmail.com>
2020-07-05 01:51:25 -0300

    Adição de data annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace webnetcore.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo é obrigatório!")]
         public string Descricao { get; set; }
 
         public List<Produto> Produtos { get; set; }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace webnetcore.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa.")]
         public int Quantidade { get; set; }
 
         public int CategoriaId { get; set; }
